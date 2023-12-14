9a2b13db01e2c0436d50e58f1ed826f36e7126c9
Igor Soares <igorgomes.13ig@gmail.com>
2023-12-14 19:59:37 -0300

    feat: add method to resolve images not found

--- src/app/pages/home/components/card-unity/card-unity.component.ts
+++ src/app/pages/home/components/card-unity/card-unity.component.ts
@@ -8,4 +8,26 @@ import { UnitLocation } from 'src/app/core/services/smart-fit.service';
 })
 export class CardUnityComponent {
   @Input() unit!: UnitLocation;
+
+  getCorrectImage(image: string): string {
+    const patternImage = {
+      forbidden: 'forbidden',
+      partial: 'partial',
+      recommended: 'recommended',
+      required: 'required',
+    }[image];
+
+    if (patternImage) {
+      return image;
+    }
+
+    const knowledgeIncorrectPattern =
+      {
+        allowed: 'required',
+        not_allowed: 'forbidden',
+        closed: 'forbidden',
+      }[image] ?? '';
+
+    return knowledgeIncorrectPattern;
+  }
 }
