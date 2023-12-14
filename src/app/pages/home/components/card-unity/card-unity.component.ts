ee5f5bf257e5f64b163e25018946a43645f4c951
Igor Soares <igorgomes.13ig@gmail.com>
2023-12-14 17:05:04 -0300

    feat: add dynamic datas in card unity

--- src/app/pages/home/components/card-unity/card-unity.component.ts
+++ src/app/pages/home/components/card-unity/card-unity.component.ts
@@ -1,12 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { mockResponseUnitsLocations } from 'src/app/core/services/responses/smart-fit.response';
 
-type Units = (typeof mockResponseUnitsLocations.locations)[0];
+type Unit = (typeof mockResponseUnitsLocations.locations)[0];
 @Component({
   selector: 'app-card-unity',
   templateUrl: './card-unity.component.html',
   styleUrls: ['./card-unity.component.sass'],
 })
 export class CardUnityComponent {
-  @Input() units: Units | {} = {};
+  @Input() unit!: Unit;
 }
